babbbb94fa0752366dd3525132bdfa51ebb74080
Nam Tran <thnam.me@gmail.com>
2021-03-23 15:14:10 -0500

    lock and unlock

--- fetchScreenshot.sh
+++ fetchScreenshot.sh
@@ -1,6 +1,31 @@
 #!/usr/bin/env bash
+LOCKDIR=/tmp/fetchScreenshot.quad.lock
+IP="192.168.1.119"
 
-while true ; do
-   lxi screenshot --address 192.168.1.119 --plugin siglent-sds scope.png
-   sleep 0.5
-done
+#Remove the lock directory
+function cleanup {
+   if rm -rf $LOCKDIR; then
+      echo "Finished"
+   else
+      echo "Failed to remove lock directory '$LOCKDIR'"
+      exit 1
+   fi
+}
+
+if mkdir $LOCKDIR; then
+   #Ensure that if we "grabbed a lock", we release it
+   #Works for SIGTERM and SIGINT(Ctrl-C)
+   trap "cleanup" EXIT
+   echo "Acquired lock, running"
+
+   while true ; do
+      lxi screenshot --address $IP --plugin siglent-sds scope.png
+      sleep 0.5
+   done
+else
+   echo "Could not create lock directory '$LOCKDIR', is another instance of"
+   echo "this script already running? If not, you can try again after manually"
+   echo "removing the lockfile $LOCKDIR." 
+   exit 1
+fi
+
