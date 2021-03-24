6e1b6a53bd216afc0dddc38fb26e17afde349bfc
Nam Tran <thnam.me@gmail.com>
2021-03-24 09:59:06 -0500

    scdp now has a lock

--- scdp.py
+++ scdp.py
@@ -7,6 +7,7 @@ from datetime import datetime, timezone
 from time import sleep
 from PIL import Image
 from io import BytesIO
+from filelock import Timeout, FileLock
 
 class GracefulKiller:
     kill_now = False
@@ -18,13 +19,19 @@ class GracefulKiller:
       print('Received SIGTERM, exiting gracefully.')
       self.kill_now = True
 
-def main(ip, output, interval):
-    killer = GracefulKiller()
+lock = '/tmp/scdp.quad.lock'
+ip = '192.168.1.119'
+output = 'scope.png'
+interval = 1.
+killer = GracefulKiller()
+
+def takeScreenshot():
     try:
         rm = pyvisa.ResourceManager()
         sds = rm.open_resource(f"TCPIP::{ip}::INSTR")
 
-        sds.chunk_size = 2*1024*1024 #default value is 20*1024(20k bytes)
+        sds.chunk_size = 2*1024*1024 
+        #default value is 20*1024(20k bytes), and the bitmap size is about 700 KB
         sds.timeout = 3000 #default value is 2000(2s)
 
         while not killer.kill_now:
@@ -42,7 +49,18 @@ def main(ip, output, interval):
             print("{} - screenshot captured.".format(utc_dt.astimezone().isoformat()))
             sleep(interval)
     except pyvisa.errors.VisaIOError:
-        print('Could not connect to device, is the IP address correct?')
+        print('Could not connect to device, is the IP address correct?',
+                file=sys.stderr)
+        exit(-1)
+    pass
+
+def main():
+    try:
+        with FileLock(lock).acquire(timeout=1):
+            takeScreenshot()
+    except Timeout:
+        print('Could not acquire lock, is there another instance of this '
+                'script running?', file=sys.stderr)
         exit(-1)
 
 if __name__ == '__main__':
@@ -51,9 +69,12 @@ if __name__ == '__main__':
     parser.add_argument('--ip', dest='ip', action='store',
             required=True, help='IP address of the scope')
     parser.add_argument('-o', '--ouput', action='store', dest='output',
-            help='Output file name', default='scope.png')
+            help='output file name', default='scope.png')
     parser.add_argument('--interval', dest='interval', action='store',
-            default=1.0, help='Interval between screenshots', type=float)
+            default=1.0, help='interval between screenshots', type=float)
+    parser.add_argument('--lock', dest='lock', action='store',
+            help='lock file', default='/tmp/scdp.quad.lock')
     args = parser.parse_args()
 
-    main(args.ip, args.output, args.interval)
+    ip, output, interval, lock = args.ip, args.output, args.interval, args.lock
+    main()
